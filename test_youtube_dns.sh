74485b9443c2a1826124982382d3cc05e8223d52
mortez <aiiniimorteza@gmail.com>
2026-02-11 09:11:05 +0100

    Create test_youtube_dns.sh

---
+++ test_youtube_dns.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+URL="https://raw.githubusercontent.com/aiiniimortez/scripts/refs/heads/main/working-dns-youtube.txt"
+TMP_FILE="/tmp/dns_list.txt"
+
+echo "[1/4] Downloading DNS list..."
+curl -fsSL "$URL" -o "$TMP_FILE"
+
+echo "[2/4] Testing DNS servers for time.ir ..."
+echo "Please wait, this may take a minute..."
+
+RESULTS=()
+
+while read -r DNS; do
+    [[ -z "$DNS" ]] && continue
+
+    TIME=$(dig time.ir @"$DNS" +tries=1 +time=2 +nocookie +norecurse 2>/dev/null \
+        | awk '/Query time:/ {print $4}')
+
+    if [[ "$TIME" =~ ^[0-9]+$ ]]; then
+        echo "OK  $DNS  ${TIME}ms"
+        RESULTS+=("$TIME $DNS")
+    else
+        echo "FAIL $DNS"
+    fi
+
+done < "$TMP_FILE"
+
+echo
+echo "[3/4] Sorting best DNS servers..."
+
+printf "%s\n" "${RESULTS[@]}" | sort -n | head -n 10 > /tmp/best_dns.txt
+
+echo
+echo "[4/4] Top 10 fastest DNS for time.ir:"
+echo "------------------------------------"
+awk '{printf "%-16s %s ms\n", $2, $1}' /tmp/best_dns.txt
