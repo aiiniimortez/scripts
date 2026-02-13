ac14335bc0f16f4df2ca72c7249de2e97842a922
mortez <aiiniimorteza@gmail.com>
2026-02-13 08:44:01 +0100

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -17,6 +17,7 @@ rm -f /etc/resolv.conf
 echo "[4/6] Write real /etc/resolv.conf ..."
 cat >/etc/resolv.conf <<EOF
 nameserver 8.8.8.8
+nameserver 1.1.1.1
 nameserver 9.9.9.9
 nameserver 80.191.40.136
 nameserver 80.191.40.146
